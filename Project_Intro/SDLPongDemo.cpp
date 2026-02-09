d16b6f9cbebe4310bd34d72a10d8da008ca12643
S277144 <s277144@uos.ac.uk>
2026-02-09 13:30:51 +0000

    Ball and Player 1 movement functional

--- Project_Intro/SDLPongDemo.cpp
+++ Project_Intro/SDLPongDemo.cpp
@@ -4,12 +4,15 @@
 
 int main()
 {
+	constexpr int ScreenWidth = 1200;
+	constexpr int ScreenHeight = 720;
+
 	SDL_Init(SDL_INIT_VIDEO);
 
 	SDL_Window* window(nullptr);
 	SDL_Renderer* renderer(nullptr);
 
-	SDL_CreateWindowAndRenderer("Pong", 1200, 720, 0, &window, &renderer);
+	SDL_CreateWindowAndRenderer("Pong", ScreenWidth, ScreenHeight, 0, &window, &renderer);
 
 	if (window == nullptr)
 	{
@@ -26,34 +29,93 @@ int main()
 		return -1;
 	}
 
-	bool running(true);
+	bool running{true};
+
+	const float PadWidth { 15.f };
+	const float PadHeight {100.f};
+
+	const float PlayerOneX = 10.0f;
+	const float PlayerTwoX = ScreenWidth - PadWidth - PlayerOneX;
+		
+	const float PlayerY = ScreenHeight / 2.0f - PadHeight / 2.0f;
+	const float PlayerSpeed{ 0.1f };
+
+	SDL_FRect player_one{ PlayerOneX, PlayerY, PadWidth, PadHeight };
+	SDL_FRect player_two{ PlayerTwoX, PlayerY, PadWidth, PadHeight };
+
+	const float BallDim { 12.5f };
+	const float BallX = { ScreenWidth / 2.0f - BallDim / 2.0f };
+	const float BallY = { ScreenHeight / 2.0f - BallDim / 2.0f };
+	const float Ballspeed{ 0.05f };
+
+	SDL_FRect ball{ BallX, BallY, BallDim, BallDim };
+
+	float POneY = PlayerY;
+	float PTwoY = PlayerY;
+	float ballX = BallX;
+	float ballY = BallY;
+
+	float velX = Ballspeed;
+	float velY = Ballspeed;;
+
+	bool isWPressed{ false };
+	bool isSPressed{ false };
 
 	while (running)
 	{
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
+
 			if (event.type == SDL_EventType::SDL_EVENT_QUIT)
 			{
 				running = false;
-					break;
-
-					if (event.type == SDL_EventType::SDL_EVENT_KEY_DOWN)
-					{
-						if (event.key.key == SDLK_ESCAPE) running = false;
-				}
+				break;
+			}
+			if (event.type == SDL_EventType::SDL_EVENT_KEY_DOWN)
+			{
+				if (event.key.key == SDLK_ESCAPE) running = false;
+				if (event.key.key == SDLK_W) isWPressed = true;
+				if (event.key.key == SDLK_S) isSPressed = true;
 			}
 
+			if (event.type == SDL_EventType::SDL_EVENT_KEY_UP)
+			{
+				if (event.key.key == SDLK_W) isWPressed = false;
+				if (event.key.key == SDLK_S) isSPressed = false;
+			}
+		}
 
+		if (isWPressed)
+		{
+			POneY -= PlayerSpeed;
+		}
+		if (isSPressed)
+		{
+			POneY += PlayerSpeed;
+		}
+
+		player_one.y = POneY;
+
+		ball.x += velX;
+		ball.y += velY;
+
+		if (ball.y <= 0 || ball.y + BallDim >= ScreenHeight)
+		{
+			velY *= -1;
 		}
 
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderClear(renderer);
 
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-		SDL_FRect rect{ 100.0f, 100.0f, 15.0f, 125.0f };
+		SDL_RenderFillRect(renderer, &player_one);
 
-		SDL_RenderFillRect(renderer, &rect);
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderFillRect(renderer, &player_two);
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderFillRect(renderer, &ball);
 
 		SDL_RenderPresent(renderer);
 	}
@@ -62,5 +124,6 @@ int main()
 	SDL_DestroyWindow(window);
 	SDL_Quit();
 
+	return 0;
 }
 
