d2388aef18d7dd7a5e838fb535c9b4d990514bb7
S277144 <s277144@uos.ac.uk>
2026-03-05 14:40:43 +0000

    Finished Brick Break

--- Project3/BrickBreakDemo.cpp
+++ Project3/BrickBreakDemo.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <SDL3\SDL.h>
+#include <format>
+#include <string>
 
 int main()
 {
 	constexpr int ScreenWidth = 1100;
-	constexpr int ScreenHeight = 1400;
+	constexpr int ScreenHeight = 1300;
 
 	SDL_Init(SDL_INIT_VIDEO);
 
@@ -40,7 +42,7 @@ int main()
 	const float PlayerX = ScreenWidth / 2.0f - PadHeight / 2.0f;
 	const float PlayerSpeed{ 0.08f };
 
-	SDL_FRect player_one{ PlayerOneX, ScreenHeight - 100, PadWidth, PadHeight };
+	SDL_FRect player_one{ PlayerOneX, ScreenHeight - 150, PadWidth, PadHeight };
 
 	const float BallDim{ 12.5f };
 	const float BallX = { ScreenWidth / 2.0f - BallDim / 2.0f };
@@ -59,15 +61,48 @@ int main()
 	bool isAPressed{ false };
 	bool isDPressed{ false };
 
+	bool flippedX{ false };
+	bool flippedY{ false };
+
 	constexpr int BlockColumns{ 10 };
-	constexpr int BlockRows{ 4 };
+	constexpr int BlockRows{ 6 };
 	constexpr float BlockGap{ 10.0f };
 
 	constexpr float BlockAreaWidth = ScreenWidth - BlockGap;
-	constexpr float BlockAreaHeight = ScreenHeight / 3.0f;
+	constexpr float BlockAreaHeight = ScreenHeight / 2.3f;
 
 	constexpr float BlockWidth = (BlockAreaWidth - BlockGap * BlockColumns) / BlockColumns;
 	constexpr float BlockHeight = BlockAreaHeight / BlockRows;
+
+	constexpr float Border = ScreenHeight - BlockAreaHeight;
+
+	int Playerlives { 3 };
+
+	bool destroyedblocks[BlockColumns * BlockRows];
+	int sz = BlockColumns * BlockRows;
+
+	// block Grid Spawning 
+
+	SDL_FRect blockRect{ BlockGap, BlockWidth, BlockHeight };
+	SDL_FRect blocks[BlockRows * BlockColumns];
+	int blockHealth[BlockRows * BlockColumns];
+
+	for (int y = 0; y < BlockRows; y++)
+	{
+		for (int x = 0; x < BlockColumns; x++)
+		{
+			int index = x + y * BlockColumns;
+			destroyedblocks[index] = false;
+
+			blocks[index].x = BlockGap + x * (BlockWidth + BlockGap);
+			blocks[index].y = BlockGap + y * (BlockHeight + BlockGap);
+
+			blocks[index].w = BlockWidth;
+			blocks[index].h = BlockHeight;
+
+			blockHealth[index] = 3;
+		}
+	}
 
 	while (running)
 	{
@@ -96,126 +131,222 @@ int main()
 			}
 		}
 
-			if (isAPressed)
+		if (isAPressed)
+		{
+			POneX -= PlayerSpeed;
+		}
+		if (isDPressed)
+		{
+			POneX += PlayerSpeed;
+		}
+
+		player_one.x = POneX;
+
+		ball.x += velX;
+		ball.y += velY;
+
+		//Stops the paddles from leaving map
+
+		if (POneX < 0)
+		{
+			POneX = 0;
+		}
+
+		if (POneX > ScreenWidth - 115)
+		{
+      		POneX = ScreenWidth - 115;
+		}
+
+		//Checking if ball and paddle is overlapping
+
+		if (ball.x <= 0 || ball.x + BallDim >= ScreenWidth || ball.y >= ScreenHeight)
+		{
+			velX *= -1;
+		}
+
+		if (SDL_HasRectIntersectionFloat(&player_one, &ball))
+		{
+			velY *= -1;
+		}
+
+		if (ball.y <= 0 )
+		{
+			velY *= -1;
+		}
+
+		// Ball respawning and lives system
+
+		if (ball.y > ScreenHeight - 100)
+		{
+			ball.x = ScreenWidth / 2.0f - BallDim / 2.0f;
+			ball.y = ScreenHeight / 2.0f - BallDim / 2.0f;
+
+			velX *= -1;
+
+			Playerlives -= 1;
+
+			std::cout << "1 life from Player\n";
+		}
+
+		// Block Grid Manager
+
+		for (int y = 0; y < BlockRows; y++)
+		{
+			for (int x = 0; x < BlockColumns; x++)
 			{
-				POneX -= PlayerSpeed;
-			}
-			if (isDPressed)
-			{
-				POneX += PlayerSpeed;
-			}
+				int index = y + x * BlockRows;
 
-			player_one.x = POneX;
-
-			ball.x += velX;
-			ball.y += velY;
-
-			//Stops the paddles from leaving map
-
-			if (POneX < 0)
-			{
-				POneX = 0;
-			}
-
-			if (POneX > ScreenWidth - 115)
-			{
-				POneX = ScreenWidth - 115;
-			}
-
-			//Checking if ball and paddle is overlapping
-
-			if (ball.x <= 0 || ball.x + BallDim >= ScreenWidth || ball.y >= ScreenHeight)
-			{
-				velX *= -1;
-			}
-
-			if (SDL_HasRectIntersectionFloat(&player_one, &ball))
-			{
-				velY *= -1;
-			}
-
-			if (ball.y <= 0 )
-			{
-				velY *= -1;
-			}
-
-			// Ball respawning
-
-			if (ball.y > ScreenHeight)
-			{
-				ball.x = ScreenWidth / 2.0f - BallDim / 2.0f;
-				ball.y = ScreenHeight / 2.0f - BallDim / 2.0f;
-
-				velX *= -1;
-			}
-
-			//Blocks spawning and functionality
-
-			SDL_FRect blockRect{BlockGap, BlockGap, BlockWidth, BlockHeight};
-			SDL_FRect blocks [BlockRows * BlockColumns];
-
-			bool destroy_blocks [BlockRows * BlockColumns];
-
-			for (int y = 0; y < BlockRows; y++)
-			{
-				for (int x = 0; x < BlockColumns; x++)
+				if (!destroyedblocks[index] && SDL_HasRectIntersectionFloat(&ball, &blocks[index]))
 				{
-					int index = y + x * BlockRows;
-					destroy_blocks[index] = false;
-
-					blocks[index].x = BlockGap + x + (BlockWidth + BlockGap);
-					blocks[index].y = BlockGap + y + (BlockHeight + BlockGap);
-
-					blocks[index].w = BlockWidth;
-					blocks[index].h = BlockHeight;
-				}
-			}
-			/*
-			player.chechAgainstBounds();
-
-			ball.rect.x += ball.vel_x;
-			ball.rect.y += ball.vel_y;
-			*/
-
-
-			for (int y = 0; y < BlockRows; y++)
-			{
-				for (int x = 0; x < BlockColumns; x++)
-				{
-					int index = y + x * BlockRows;
-					
-					if (!destroy_blocks[index] && SDL_HasRectIntersectionFloat(&ball, &blocks[index]))
+					if (ball.x < blocks[index].x || ball.x + BallDim > blocks[index].x + BlockWidth)
 					{
-						destroy_blocks[index] = true;
-						if (ball.x < blocks[index].x || ball.x > blocks[index].x + BlockWidth)
-						{
-							velX *= -1;
-
-							velY *= -1;
-						}
+						velX *= -1;
+						flippedX = true;
 					}
+
+					if (ball.y < blocks[index].y || ball.y + BallDim > blocks[index].y + BlockHeight)
+					{
+						velY *= -1;
+						flippedY = true;
+					}
+
+					blockHealth[index] -= 1;
+
+					if (blockHealth[index] == 0)
+					{
+						destroyedblocks[index] = true;
+					}
+
+					std::string log = std::format("ball (pos {}, {}) (vel {}, {}) intersected brick [{}] x:{}, {}, y: {}, {} - flipped x {}, flipped y {}\n",
+						ball.x, ball.y, velX, velY, index, blocks[index].x, blocks[index].x + BlockWidth, blocks[index].y, blocks[index].y + BlockHeight, flippedX, flippedY);
 				}
 			}
+		}
 
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-			SDL_RenderClear(renderer);
+		// End state
 
-			SDL_SetRenderDrawColor(renderer, 43, 250, 140, 255);
-			SDL_RenderFillRect(renderer, &player_one);
+		bool gameOvergood{ true };
+		for (int i = 0; i < sz; i++)
+		{
+			if (!destroyedblocks[i]) 
+			{
+				gameOvergood = false;
+				break;
+			}
+		}
 
-			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-			SDL_RenderFillRect(renderer, &ball);
+		// Block lives system
 
-			SDL_SetRenderDrawColor(renderer, 222, 88, 61, 255);
-			SDL_RenderFillRect(renderer, &blockRect);
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+		SDL_RenderClear(renderer);
 
-			SDL_RenderPresent(renderer);
+		for (int y = 0; y < BlockRows; y++)
+		{
+			for (int x = 0; x < BlockColumns; x++)
+			{
+				int index = y + x * BlockRows;
+
+				if (blockHealth[index] == 3)
+				{
+					SDL_SetRenderDrawColor(renderer, 222, 88, 61, 255);
+				}
+
+				if (blockHealth[index] == 2)
+				{
+					SDL_SetRenderDrawColor(renderer, 236, 201, 73, 255);
+				}
+
+				if (blockHealth[index] == 1)
+				{
+					SDL_SetRenderDrawColor(renderer, 80, 235, 183, 255);
+				}
+
+				if (destroyedblocks[index])continue;
+
+				SDL_RenderFillRect(renderer, &blocks[index]);
+			}
+		}
+
+		// End state and rendering 
+
+		SDL_FRect border{ 0, ScreenHeight - 100.0f, ScreenWidth, 100.0f };
+		SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+		SDL_RenderFillRect(renderer, &border);
+
+		SDL_FRect pointone{ 100, ScreenHeight - 100, 40.0f, ScreenHeight };
+		SDL_SetRenderDrawColor(renderer, 80, 235, 183, 255);
+		SDL_RenderFillRect(renderer, &pointone);
+
+		SDL_FRect pointtwo{ 200, ScreenHeight - 100, 40.0f, ScreenHeight};
+		SDL_SetRenderDrawColor(renderer, 80, 235, 183, 255);
+		SDL_RenderFillRect(renderer, &pointtwo);
+
+		SDL_FRect pointthree{ 300, ScreenHeight - 100, 40.0f, ScreenHeight };
+		SDL_SetRenderDrawColor(renderer, 80, 235, 183, 255);
+		SDL_RenderFillRect(renderer, &pointthree);
+
+		SDL_SetRenderDrawColor(renderer, 80, 235, 183, 255);
+		SDL_RenderFillRect(renderer, &player_one);
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+		SDL_RenderFillRect(renderer, &ball);
+
+		if (gameOvergood)
+		{
+			SDL_FRect EndG{ 0, 0, BlockAreaWidth, ScreenHeight - 100 };
+			SDL_SetRenderDrawColor(renderer, 141, 247, 41, 255);
+			SDL_RenderFillRect(renderer, &EndG);
+
+			velX = 0;
+		}
+
+		if (Playerlives == 2)
+		{
+			SDL_FRect pointthree{ 300, ScreenHeight - 100, 40.0f, ScreenHeight };
+			SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+			SDL_RenderFillRect(renderer, &pointthree);
+		}
+
+		if (Playerlives == 1)
+		{
+			SDL_FRect pointtwo{ 200, ScreenHeight - 100, 40.0f, ScreenHeight };
+			SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+			SDL_RenderFillRect(renderer, &pointtwo);
+
+			SDL_FRect pointthree{ 300, ScreenHeight - 100, 40.0f, ScreenHeight };
+			SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+			SDL_RenderFillRect(renderer, &pointthree);
+		}
+
+		if (Playerlives == 0)
+		{
+			SDL_FRect Endb{ 0, 0, BlockAreaWidth, ScreenHeight - 100 };
+			SDL_SetRenderDrawColor(renderer, 224, 31, 17, 255);
+			SDL_RenderFillRect(renderer, &Endb);
+
+			SDL_FRect pointone{ 100, ScreenHeight - 100, 40.0f, ScreenHeight };
+			SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+			SDL_RenderFillRect(renderer, &pointone);
+
+			SDL_FRect pointtwo{ 200, ScreenHeight - 100, 40.0f, ScreenHeight };
+			SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+			SDL_RenderFillRect(renderer, &pointtwo);
+
+			SDL_FRect pointthree{ 300, ScreenHeight - 100, 40.0f, ScreenHeight };
+			SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+			SDL_RenderFillRect(renderer, &pointthree);
+
+			velX = 0;
+			velY = 0;
+		}
+
+		SDL_RenderPresent(renderer);
 
     }
 
-		SDL_DestroyRenderer(renderer);
-		SDL_DestroyWindow(window);
-		SDL_Quit();
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 
-		return 0;
+	return 0;
 }
